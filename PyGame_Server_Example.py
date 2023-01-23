f62a76cf5539af26097d16657d70b43912600f52
Jay C33 <85657500+jayc3-3@users.noreply.github.com>
2023-01-23 02:36:41 -0800

    Update PyGame_Server_Example.py

--- PyGame_Server_Example.py
+++ PyGame_Server_Example.py
@@ -11,16 +11,47 @@ print("Server setup complete")
 #Variables
 PlayerAmount = 0
 running = True
-Player1Name = ""
+P1X = False
+P1Y = False
+P2X = False
+P2Y = False
+Player1X = 50
+Player1Y = 50
+Player2X = 200
+Player2Y = 200
 Player1Addr = ""
-Player2Name = ""
 Player2Addr = ""
 
 #Netcode
 print("Server started")
 while running:
     LatestRecievedData, Client_Address = Server.recvfrom(1024)
-    LatestRecievedDataStr = str(LatestRecievedData, 'utf-8')
+    LatestRecievedDataStr = str(LatestRecievedData, 'utf-8')    
+    
+    if LatestRecievedDataStr == "Player1Left":
+        Player1X -= 5
+    
+    if LatestRecievedDataStr == "Player1Right":
+        Player1X += 5
+        
+    if LatestRecievedDataStr == "Player1Up":
+        Player1Y -= 5
+        
+    if LatestRecievedDataStr == "Player1Down":
+        Player1Y += 5
+
+    if LatestRecievedDataStr == "Player2Left":
+        Player2X -= 5
+    
+    if LatestRecievedDataStr == "Player2Right":
+        Player2X += 5
+        
+    if LatestRecievedDataStr == "Player2Up":
+        Player2Y -= 5
+        
+    if LatestRecievedDataStr == "Player2Down":
+        Player2Y += 5
+    
     if LatestRecievedDataStr == "AttemptJoin":
         if PlayerAmount == 0:
             Player1Addr = Client_Address
@@ -44,8 +75,43 @@ while running:
             DataToSend = "ServerFull"
             Server.sendto(DataToSend.encode(), Client_Address)
             print("Client attempted to join while server was full")
+
+    if PlayerAmount == 2:
+        if P1X == True:
+            Player1X = LatestRecievedDataStr
+            DataToSend = str(Player1X)
+            Server.sendto(DataToSend.encode(), Player2Addr)
+            P1X = False
+
+        if P1Y == True:
+            Player1Y = LatestRecievedDataStr
+            DataToSend = str(Player1Y)
+            Server.sendto(DataToSend.encode(), Player2Addr)
+            P1Y = False
+        
+        if P2X == True:
+            Player2X = LatestRecievedDataStr
+            P2X = False
+
+        if P2Y == True:
+            Player2Y = LatestRecievedDataStr
+            DataToSend = str(Player2Y)
+            Server.sendto(DataToSend.encode(), Player1Addr)
+            P2Y = False
+
+        if LatestRecievedDataStr == "Player1PositionX":
+            P1X = True
+
+        if LatestRecievedDataStr == "Player1PositionY":
+            P1Y = True
+        
+        if LatestRecievedDataStr == "Player2PositionX":
+            P2X = True
+        
+        if LatestRecievedDataStr == "Player2PositionY":
+            P2Y = True
     
-    elif LatestRecievedDataStr == "Shutdown":
+    if LatestRecievedDataStr == "Shutdown":
         if PlayerAmount == 0:
             print("Recieved shutdown order; shutting down server")
             running = False
@@ -66,22 +132,54 @@ while running:
             print("Told player 2 server is shutting down")
             running = False
     
-    elif LatestRecievedDataStr == "P1Disconnect":
+    if LatestRecievedDataStr == "P1Disconnect":
+        Player1X = 50
+        Player1Y = 50
         if PlayerAmount == 2:
             DataToSend = "Player1DC"
             Server.sendto(DataToSend.encode(), Player2Addr)
             Player1Addr = ""
             Player2Addr = ""
             PlayerAmount = 0
-            print("Player 1 Disconnected; also kicked player 2")
+            Player2X = 200
+            Player2Y = 200
+            print("Player 1 disconnected; also kicked player 2")
+        
+        else:
+            Player1Addr = ""
+            PlayerAmount = 0
+            print("Player 1 disconnected")
 
-    elif LatestRecievedDataStr == "P2Disconnect":
+    if LatestRecievedDataStr == "P2Disconnect":
         if PlayerAmount == 2:
             DataToSend = "Player2DC"
             Server.sendto(DataToSend.encode(), Player1Addr)
             Player1Addr = ""
             Player2Addr = ""
             PlayerAmount = 0
-            print("Player 2 Disconnected; also kicked player 1")
+            Player1X = 50
+            Player1Y = 50
+            Player2X = 200
+            Player2Y = 200
+            print("Player 2 disconnected; also kicked player 1")
+    
+    if PlayerAmount == 2:
+        DataToSend = "Player1X"
+        Server.sendto(DataToSend.encode(), Player2Addr)
+        DataToSend = str(Player1X)
+        Server.sendto(DataToSend.encode(), Player2Addr)
+        DataToSend = "Player1Y"
+        Server.sendto(DataToSend.encode(), Player2Addr)
+        DataToSend  = str(Player1Y)
+        Server.sendto(DataToSend.encode(), Player2Addr)
 
+        DataToSend = "Player2X"
+        Server.sendto(DataToSend.encode(), Player1Addr)
+        DataToSend = str(Player2X)
+        Server.sendto(DataToSend.encode(), Player1Addr)
+        DataToSend = "Player2Y"
+        Server.sendto(DataToSend.encode(), Player1Addr)
+        DataToSend  = str(Player2Y)
+        Server.sendto(DataToSend.encode(), Player1Addr)
+    
 print("Server stopped")
